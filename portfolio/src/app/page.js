5fc8edcc7b1b96cd895b08cc8d5d47c6606ee4d5
Manyagkarle13 <manyagkarle@gmail.com>
2026-04-13 22:33:52 +0530

    "Initial commit"

---
+++ portfolio/src/app/page.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+      
+      <h1 className="text-5xl font-bold">
+        Hi, I'm Manya 👋
+      </h1>
+
+      <p className="mt-4 text-xl">
+        Information Science Student | Aspiring Software Developer
+      </p>
+
+      <button className="mt-6 px-6 py-3 bg-blue-600 rounded-lg hover:bg-blue-700">
+        View My Projects
+      </button>
+
+    </main>
+  );
+}
